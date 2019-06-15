29d49d5efc3f9aff1c042f783b4a72713e6135ac
Jacques Tronconi <jacques.tronconi@gmail.com>
2019-06-16 00:49:43 +0200

    Initial Commit

---
+++ ATest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ATest
+//
+//  Created by jacques tronconi on 16/06/2019.
+//  Copyright © 2019 jacques tronconi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
